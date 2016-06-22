95ff4d8a5869db7d30382a903d67e5f16a94a1cc
zbw322092 <bz4e14@soton.ac.uk>
2016-06-22 14:35:01 +0800

    input ngModel

--- ngModel and input/script.js
+++ ngModel and input/script.js
@@ -3,9 +3,15 @@ angular.module('ngModelExample', [])
 		$scope.booleanValue = true;
 		$scope.secondValue = "YES";
 	}])
-	.controller('ExampleController', ['$scope', function($scope) {
+	.controller('InputTextController', ['$scope', function($scope) {
 		$scope.example = {
 			text: 'guest',
 			word: /^\s*\w*\s*$/
+		};
+	}])
+	.controller('InputController', ['$scope', function($scope) {
+		$scope.inputExample = {
+			name: 'guest',
+			last: 'visitor'
 		};
 	}]);
