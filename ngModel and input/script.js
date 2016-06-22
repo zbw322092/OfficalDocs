6d03e7bd309c7aad2d0a295b9704f01e4c31f83e
zbw322092 <bz4e14@soton.ac.uk>
2016-06-22 11:09:30 +0800

    ngModel

--- ngModel and input/script.js
+++ ngModel and input/script.js
@@ -2,4 +2,10 @@ angular.module('ngModelExample', [])
 	.controller('CheckboxController', ['$scope', function($scope){
 		$scope.booleanValue = true;
 		$scope.secondValue = "YES";
+	}])
+	.controller('ExampleController', ['$scope', function($scope) {
+		$scope.example = {
+			text: 'guest',
+			word: /^\s*\w*\s*$/
+		};
 	}]);
