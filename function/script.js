f4e1b5f8f993ff0eeb1a8d10cf1675cb9a9cac65
zbw322092 <bz4e14@soton.ac.uk>
2016-07-05 10:42:17 +0800

    angular copy extend merge compare

--- function/script.js
+++ function/script.js
@@ -66,40 +66,147 @@ app.controller('CopyController', function($scope) {
 })
 
 .controller('FromJsonController', function($scope) {
-	$scope.jsonExample = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+  $scope.jsonExample = '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
-	$scope.jsonValue = angular.fromJson($scope.jsonExample);
-	// console.log(typeof $scope.jsonValue); // object
-	// console.log(typeof $scope.jsonExample); // string
-	
-	$scope.typeOrigin = typeof $scope.jsonExample;
-	$scope.typeConvert = typeof $scope.jsonValue;
+  $scope.jsonValue = angular.fromJson($scope.jsonExample);
+  // console.log(typeof $scope.jsonValue); // object
+  // console.log(typeof $scope.jsonExample); // string
+
+  $scope.typeOrigin = typeof $scope.jsonExample;
+  $scope.typeConvert = typeof $scope.jsonValue;
 })
 
 .controller('IdentityExample', function($scope) {
-	function getResult(fn, input) {
-		return (fn || angular.identity)(input);
-	}
+  function getResult(fn, input) {
+    return (fn || angular.identity)(input);
+  }
 
-	console.log(getResult(function(n) {return n * 2}, 21));
-	console.log(getResult(null, 21));
-	console.log(getResult(undefined, 21));
+  console.log(getResult(function(n) {
+    return n * 2 }, 21));
+  console.log(getResult(null, 21));
+  console.log(getResult(undefined, 21));
+})
+
+.controller('CompareExample', function($scope) {
+  // angular.copy is deep copy
+  var o1 = {
+    name: 'bowen',
+    age: 23,
+    other: {}
+  }
+  var o2 = angular.copy(o1);
+  console.log(o2);
+  console.log(o1 === o2); // false
+  console.log(o1 == o2); // false
+  console.log(o1.name === o2.name); // true
+  console.log(o1.other === o2.other); // false
+
+  var oo1 = [1, 2, 3, 4];
+  var oo2 = angular.copy(oo1);
+
+  console.log(oo2);
+  console.log(oo1 === oo2); // false
+  console.log(oo1[0] === oo2[0]); // true
+
+  var s = 'it is a string';
+  var ss = angular.copy(s);
+  console.log(ss);
+  console.log(s === ss); // true
+
+  // angular.extend is shallow copy
+  var src1 = {
+    name: 'bowen',
+    age: 23,
+    other: {}
+  },
+  src2 = {
+    name: 'huihui',
+    height: 160,
+  },
+  dst = {};
+  angular.extend(dst, src1, src2);
+
+  console.log(dst);
+  console.log(dst.name === src2.name); // true
+  console.log(dst.other === src1.other); // true
+
+  var src1 = { skill: { name: 'Java', experience: 20, certified: true } };
+  var src2 = { skill: { name: 'JavaScript', experience: 10 } };
+  var dst = {};
+
+  console.log(angular.extend(dst, src1, src2)); // certified is lost in this case, because 
+  // angular extend method dose not recursively copying the individual properties inside 
+  // the skill value.
+  console.log(dst.skill);
+  console.log(dst.skill === src2.skill);
+  console.log(dst.skill.name === src2.skill.name);
+
+
+
+
+  // angular.merge is deep copy
+  var src1 = {
+    name: 'bowen',
+    age: 23,
+    other: {}
+  },
+  src2 = {
+    name: 'huihui',
+    height: 160
+  },
+  dst = {}; 
+
+  angular.merge(dst, src1, src2);
+  console.log(dst);
+
+  var src3 = {
+    person:
+    {
+      name: 'bowen',
+      age: 23,
+      other: {}
+    }
+  },
+  src4 = {
+    person:
+    {
+      name: 'huihui',
+      height: 160      
+    }
+  },
+  dst2 = {};
+  console.log(angular.merge(dst2, src3, src4));
+  console.log(dst2.person.name === src4.person.name); // true
+  console.log(dst2.person.other === src3.person.other); // false
+
+
+  var src5 = {
+    person:
+    {
+      name: 'bowen',
+      age: 23,
+      other: {}
+    }
+  },
+  src6 = {
+    person2:
+    {
+      name: 'huihui',
+      height: 160      
+    }
+  },
+  dst3 = {};
+  // console.log(angular.extend(dst3, src5, src6)); // the rusult looks the same as merge in this 
+  // // situation, however, extend is shallow copy, so the result of next link of code is true.
+  // console.log(dst3.person.other === src5.person.other); // true
+  console.log(angular.merge(dst3, src5, src6));
+  console.log(dst3.person.other === src5.person.other); // false
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
